687e09f24845c067951e1b3a4c9f4d60e73974ec
Adebola Akeredolu <debola31@gmail.com>
2016-01-07 19:06:16 +0100

    Update Battleship.py
    Update the battleship game to allow for 2 battleships to exist on the board, with the condition of completion being the discovery of both battleships

--- Battleship.py
+++ Battleship.py
@@ -1,9 +1,12 @@
-from random import randint
+import random
 
 board = []
+ship_row =[]
+ship_col = []
+ship_status = [False,False]
 
-for x in range(6):
-    board.append(["O"] * 6)
+for x in range(5):
+    board.append(["O"] * 5)
 
 def print_board(board):
     for row in board:
@@ -13,37 +16,110 @@ print ("Let's play Battleship!")
 print_board(board)
 
 def random_row(board):
-    return randint(0, len(board) - 1)
+    return random.randint(0, len(board) - 1)
 
 def random_col(board):
-    return randint(0, len(board[0]) - 1)
+    return random.randint(0, len(board[0]) - 1)
 
-ship_row = random_row(board)
-ship_col = random_col(board)
+for i in range(2):
+    ship_row.append(random_row(board))
+    ship_col.append(random_col(board))
 
-limit = 4
+if ship_row[0] == ship_row[1] and ship_col[0] == ship_col[1]:
+    ship_row[1] = random.choice(range(0,ship_row[0]) + range(ship_row[0]+ 1,5))
+    ship_col[1] = random.choice(range(0,ship_col[0]) + range(ship_col[0]+ 1,5))
+
+
+for i in range(2):
+    print (ship_row[i])
+    print (ship_col[i])
+    
+limit = 5
 
 for turn in range(limit):
     print ('Turn', turn + 1)
     guess_row = int(input("Guess Row:"))
     guess_col = int(input("Guess Col:"))
 
-    if guess_row == ship_row and guess_col == ship_col:
-        print ("Congratulations! You sunk my battleship!")
-        break
-    elif guess_row not in range(6) or guess_col not in range(6):
-        print ("Oops, that's not even in the ocean.")
-        if turn == limit - 1:
-            print ('Game Over')
-    elif(board[guess_row][guess_col] == "X"):
-        print ("You guessed that one already.")
-        if turn == limit - 1:
-            print ('Game Over')
-    else:
-        print ("You missed my battleship!")
-        board[guess_row][guess_col] = "X"
-        if turn == limit - 1:
-            print ('Game Over')
-        
-    if not turn == limit - 1:
-        print_board(board)
+    if ship_status[0] == False and ship_status[1] == False:
+        if guess_row == ship_row[0] and guess_col == ship_col[0]:
+            board[guess_row][guess_col] = "X"
+            print ("Congratulations! You sunk a battleship!")
+            ship_status[0] = True
+            if ship_status[0] == True and ship_status[1] == True:
+                print ('Game Won')
+                break
+        elif guess_row == ship_row[1] and guess_col == ship_col[1]:
+            board[guess_row][guess_col] = "X"
+            print ("Congratulations! You sunk a battleship!")
+            ship_status[1] = True
+            if ship_status[0] == True and ship_status[1] == True:
+                print ('Game Won')
+                break
+        elif guess_row not in range(5) or guess_col not in range(5):
+            print ("Oops, that's not even in the ocean.")
+            if turn == limit - 1:
+                 print ('Game Over')
+        elif(board[guess_row][guess_col] == "X"):
+            print ("You guessed that one already.")
+            if turn == limit - 1:
+                print ('Game Over')
+        else:
+            print ("You missed my battleship!")
+            board[guess_row][guess_col] = "X"
+            if turn == limit - 1:
+                  print ('Game Over')
+                
+        if not turn == limit - 1:
+            print_board(board)
+
+    elif ship_status[0] == True and ship_status[1] == False:
+        if guess_row == ship_row[1] and guess_col == ship_col[1]:
+            board[guess_row][guess_col] = "X"
+            print ("Congratulations! You sunk a battleship!")
+            ship_status[1] = True
+            if ship_status[0] == True and ship_status[1] == True:
+                print ('Game Won')
+                break
+        elif guess_row not in range(5) or guess_col not in range(5):
+            print ("Oops, that's not even in the ocean.")
+            if turn == limit - 1:
+                 print ('Game Over')
+        elif(board[guess_row][guess_col] == "X"):
+            print ("You guessed that one already.")
+            if turn == limit - 1:
+                print ('Game Over')
+        else:
+            print ("You missed my battleship!")
+            board[guess_row][guess_col] = "X"
+            if turn == limit - 1:
+                  print ('Game Over')
+                
+        if not turn == limit - 1:
+            print_board(board)
+
+    elif ship_status[0] == False and ship_status[1] == True:
+        if guess_row == ship_row[0] and guess_col == ship_col[0]:
+            board[guess_row][guess_col] = "X"
+            print ("Congratulations! You sunk a battleship!")
+            ship_status[1] = True
+            if ship_status[0] == True and ship_status[1] == True:
+                print ('Game Won')
+                break
+        elif guess_row not in range(5) or guess_col not in range(5):
+            print ("Oops, that's not even in the ocean.")
+            if turn == limit - 1:
+                 print ('Game Over')
+        elif(board[guess_row][guess_col] == "X"):
+            print ("You guessed that one already.")
+            if turn == limit - 1:
+                print ('Game Over')
+        else:
+            print ("You missed my battleship!")
+            board[guess_row][guess_col] = "X"
+            if turn == limit - 1:
+                  print ('Game Over')
+                
+        if not turn == limit - 1:
+            print_board(board)
+
